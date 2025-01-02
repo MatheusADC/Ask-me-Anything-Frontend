607d7a81f0e0c2eae9738e8f4c85392d58a40415
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-01-02 06:16:59 -0300

    Remove: Comment

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// importação nomeada coloca as chaves em volta
 import { App } from './app.tsx'
 import './index.css'
 
